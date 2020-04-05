549c09845dcbde47d2f1a6c40d7dfd2fb70ac5c3
Fritz Lin <uxfritz@163.com>
2020-04-05 15:40:29 +0800

    Updates

---
+++ precache-manifest.d62042d2ce1e03eed56c19d28a8c2d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ee1989c63737b18ca6f7647b531b47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "baa20dad5eadc11058ab",
+    "url": "./static/css/main.8bf90a64.chunk.css"
+  },
+  {
+    "revision": "480c3d70a754ccdd39e3",
+    "url": "./static/js/2.78043f34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.78043f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa20dad5eadc11058ab",
+    "url": "./static/js/main.01e544cc.chunk.js"
+  },
+  {
+    "revision": "f9fe788f41875a63b54a",
+    "url": "./static/js/runtime-main.8fa88054.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
